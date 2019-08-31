462f1efaf7a68176623b102f979ab3ad81c27a19
Alp Bintuğ Uzun <48649717+alpbintug@users.noreply.github.com>
2019-08-31 22:52:25 +0300

    Create Class.java

---
+++ E004-Multi-Class-Program/Class.java
@@ -0,0 +1,29 @@
+package ex004;
+
+import java.util.LinkedList;
+
+public class Class {
+	LinkedList<Student> students;
+	String className;
+	public Class(LinkedList<Student> students, String className) {
+		students = new LinkedList<Student>();
+		this.students = students;
+		this.className = className;
+	}
+	public Class() {
+		students = new LinkedList<Student>();
+	}
+	public LinkedList<Student> getStudents() {
+		return students;
+	}
+	public void setStudents(LinkedList<Student> students) {
+		this.students = students;
+	}
+	public String getClassName() {
+		return className;
+	}
+	public void setClassName(String className) {
+		this.className = className;
+	}
+	
+}
