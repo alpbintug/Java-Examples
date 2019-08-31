e9cee9556b0dc7788da50732d156c76e0d103f0e
Alp Bintuğ Uzun <48649717+alpbintug@users.noreply.github.com>
2019-08-31 22:57:44 +0300

    Update Student.java

--- E004-Multi-Class-Program/Student.java
+++ E004-Multi-Class-Program/Student.java
@@ -3,7 +3,6 @@ package ex004;
 public class Student {
 	private String name;
 	private String ID;
-	private String _class;
 	public String getName() {
 		return name;
 	}
@@ -16,18 +15,12 @@ public class Student {
 	public void setID(String iD) {
 		ID = iD;
 	}
-	public String get_class() {
-		return _class;
-	}
-	public void set_class(String _class) {
-		this._class = _class;
-	}
+
 	public Student() {
 	}
-	public Student(String name, String iD, String _class) {
+	public Student(String name, String iD) {
 		this.name = name;
 		ID = iD;
-		this._class = _class;
 	}
 	
 
