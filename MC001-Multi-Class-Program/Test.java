b6adb5e02098409a980f5aa4c49a3108ac404cb5
Alp Bintuğ Uzun <48649717+alpbintug@users.noreply.github.com>
2019-08-31 23:06:12 +0300

    Create Test.java

---
+++ MC001-Multi-Class-Program/Test.java
@@ -0,0 +1,23 @@
+package ex004;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Student student1 = new Student();
+		student1.setID("1249143");
+		student1.setName("Leeroy Jenkins");
+		
+		Student student2 = new Student("Zula", "0142069");
+
+		Class scienceClass = new Class("Science Class");
+		scienceClass.students.add(student1);
+		scienceClass.students.add(student2);
+		
+		System.out.println("Class name: " + scienceClass.className);
+		System.out.println("Students:");
+		for (Student student : scienceClass.students) {
+			System.out.println(student.getName());
+		}
+	}
+
+}
