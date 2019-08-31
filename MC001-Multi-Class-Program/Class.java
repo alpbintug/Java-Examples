4358290140b85f13096e3053be7477301307678f
Alp Bintuğ Uzun <48649717+alpbintug@users.noreply.github.com>
2019-08-31 23:07:52 +0300

    Update Class.java

--- MC001-Multi-Class-Program/Class.java
+++ MC001-Multi-Class-Program/Class.java
@@ -6,12 +6,15 @@ public class Class {
 	LinkedList<Student> students;
 	String className;
 	public Class(LinkedList<Student> students, String className) {
-		students = new LinkedList<Student>();
+		students = new LinkedList<Student>(); //You have to allocate memory in order to use "students" linked list, otherwise you will get an exception.
 		this.students = students;
 		this.className = className;
 	}
+	public Class(String className) {
+		this.className = className;
+	}
 	public Class() {
-		students = new LinkedList<Student>();
+		students = new LinkedList<Student>(); //Yes. You have to allocate memory in all constructors.
 	}
 	public LinkedList<Student> getStudents() {
 		return students;
@@ -25,9 +28,6 @@ public class Class {
 	public void setClassName(String className) {
 		this.className = className;
 	}
-	public Class(String className) {
-		super();
-		this.className = className;
-	}
+
 	
 }
